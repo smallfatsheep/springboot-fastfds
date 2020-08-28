ca270d4c4d25b0ae1cfd94184b113df371eba3a1
huangwc <huangwencon@foxmail.com>
2020-08-28 18:39:22 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootfastfds/SpringbootFastfdsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootfastfds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFastfdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFastfdsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/springbootfastfds/fdfs/FdfsTest.java
@@ -0,0 +1,48 @@
+package com.example.springbootfastfds.fdfs;
+
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ * @Author: huangwc
+ * @Description:
+ * @Date: 2020/08/28 18:04:51
+ * @Version: 1.0
+ */
+public class FdfsTest {
+
+    @Autowired
+    private FastFileStorageClient storageClient;
+
+
+    @Test
+    public void testUpload() throws FileNotFoundException {
+        File file = new File("D:\\img.png");
+        // 上传并且生成缩略图
+        StorePath storePath = this.storageClient.uploadFile(
+                new FileInputStream(file), file.length(), "png", null);
+        // 带分组的路径
+        System.out.println(storePath.getFullPath());
+        // 不带分组的路径
+        System.out.println(storePath.getPath());
+    }
+
+    @Test
+    public void testUploadAndCreateThumb() throws FileNotFoundException {
+        File file = new File("D:\\img.png");
+        // 上传并且生成缩略图
+        StorePath storePath = this.storageClient.uploadFile(
+                new FileInputStream(file), file.length(), "png", null);
+        // 带分组的路径
+        System.out.println(storePath.getFullPath());
+        // 不带分组的路径
+        System.out.println(storePath.getPath());
+    }
+}
+
